1d7d458957d51a66cd33c9be0965551a418bb3e0
LucasChung <NEXT16212@edu.nextkbh.dk>
2025-05-19 08:41:43 +0200

    Forenklet og boids virker nu

--- OOP akvarie Eksamen/sketch.js
+++ OOP akvarie Eksamen/sketch.js
@@ -1,88 +1,48 @@
 class Vector {
-  #startX;
-  #startY;
-  #compX;
-  #compY;
-
-  constructor(startX, startY, compX, compY) {
-    this.#startX = startX;
-    this.#startY = startY;
-    this.#compX = compX;
-    this.#compY = compY;
-  }
-
-  get startX() {
-    return this.#startX;
-  }
-
-  get startY() {
-    return this.#startY;
-  }
-
-  get compX() {
-    return this.#compX;
-  }
-
-  get compY() {
-    return this.#compY;
-  }
-
-  get length(){
-    return Math.sqrt(this.#compX**2 + this.#compY**2)
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
   add(other) {
-    return new Vector(
-      this.startX + other.startX,
-      this.startY + other.startY,
-      this.compX + other.compX,
-      this.compY + other.compY
-    );
+    return new Vector(this.x + other.x, this.y + other.y);
   }
 
   subtract(other) {
-    return new Vector(
-      this.startX - other.startX,
-      this.startY - other.startY,
-      this.compX - other.compX,
-      this.compY - other.compY
-    );
+    return new Vector(this.x - other.x, this.y - other.y);
   }
-  div(n){
-    return new Vector(
-      this.startX ,
-      this.startY ,
-      this.compX / n,
-      this.compY / n
-    );
+
+  div(n) {
+    return new Vector(this.x / n, this.y / n);
   }
+
   normalize() {
-    let len = this.length;
-    if (len === 0) return new Vector(this.startX, this.startY, 0, 0);
-    return new Vector(
-      this.startX,
-      this.startY,
-      this.compX / len,
-      this.compY / len
-    );
+    let len = this.length();
+    if (len === 0) return new Vector(0, 0);
+    return new Vector(this.x / len, this.y / len);
+  }
+
+  length() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
   }
 }
 
 
 class Fish {
-  #pos
-  #vel
-  #color
-  #fishWidth
-  #fishHeight
-     constructor(pos, vel, color, fishWidth, fishHeight) {
-       this.#pos = pos;
-       this.#vel = vel;
-       this.#color = color;
-       this.#fishWidth = fishWidth;
-       this.#fishHeight = fishHeight;
-     } 
-     
+  #pos;
+  #vel;
+  #color;
+  #fishWidth;
+  #fishHeight;
+
+  constructor(pos, vel, color, fishWidth, fishHeight) {
+    this.#pos = pos;
+    this.#vel = vel;
+    this.#color = color;
+    this.#fishWidth = fishWidth;
+    this.#fishHeight = fishHeight;
+  }
+
   get pos() {
     return this.#pos;
   }
@@ -102,151 +62,132 @@ class Fish {
   get fishHeight() {
     return this.#fishHeight;
   }
+
   set vel(newVel) {
     this.#vel = newVel;
   }
+
   set pos(newPos) {
     this.#pos = newPos;
   }
 
-     show(){
-       fill(this.#color)
-       triangle(this.#pos.startX, this.#pos.startY, 
-               this.#pos.startX-this.#fishWidth, this.#pos.startY-this.#fishHeight/2, 
-               this.#pos.startX-this.#fishWidth, this.#pos.startY+this.#fishHeight/2);
-       ellipse(this.#pos.startX,this.#pos.startY,this.#fishWidth,this.#fishHeight)
-       fill("white")
-       rect(this.#pos.startX-this.#fishWidth/8, this.#pos.startY-this.#fishHeight/2,this.#fishWidth/8, this.#fishHeight)
-       line(this.#pos.startX,this.#pos.startY,this.#pos.startX+this.#fishWidth/2,this.#pos.startY)
-       circle(this.#pos.startX+this.#fishWidth/4,this.#pos.startY-this.#fishHeight/4,3)
-     }
-     update(){
-       this.#pos = this.#pos.add(this.#vel)
-   }
+  show() {
+    fill(this.#color);
+    triangle(
+      this.#pos.x, this.#pos.y,
+      this.#pos.x - this.#fishWidth, this.#pos.y - this.#fishHeight / 2,
+      this.#pos.x - this.#fishWidth, this.#pos.y + this.#fishHeight / 2
+    );
+    ellipse(this.#pos.x, this.#pos.y, this.#fishWidth, this.#fishHeight);
+    fill("white");
+    rect(this.#pos.x - this.#fishWidth / 8, this.#pos.y - this.#fishHeight / 2, this.#fishWidth / 8, this.#fishHeight);
+    line(this.#pos.x, this.#pos.y, this.#pos.x + this.#fishWidth / 2, this.#pos.y);
+    circle(this.#pos.x + this.#fishWidth / 4, this.#pos.y - this.#fishHeight / 4, 3);
+  }
 }
 
 
 class PreyFish extends Fish {
   constructor() {
-    let pos = new Vector(
-      random(40,width-40),
-      random(40,height-40),
-      random(40,width-40),
-      random(40,height-40)
-    );
-    let vel = new Vector(
-      0,
-      0,
-      100,
-      100
-    );
+    let pos = new Vector(random(40, width - 40), random(40, height - 40));
+    let vel = new Vector(random(-2, 2), random(-2, 2));
     super(pos, vel, "orange", 40, 20);
   }
 
   cohesion(fishArray) {
-    let pCenter = new Vector(0, 0, 0, 0);
+    let pCenter = new Vector(0, 0);
     let total = 0;
-  
+
     for (let other of fishArray) {
       if (other !== this) {
         pCenter = pCenter.add(other.pos);
         total++;
       }
     }
-  
+
     if (total > 0) {
-      pCenter = new Vector(pCenter.startX / total, pCenter.startY / total, 0, 0);
-  
-      let steerX = (pCenter.startX - this.pos.startX) / 100;
-      let steerY = (pCenter.startY - this.pos.startY) / 100;
-  
-      return new Vector(0, 0, steerX, steerY);
+      pCenter = pCenter.div(total);
+      let steer = new Vector(
+        (pCenter.x - this.pos.x) / 100,
+        (pCenter.y - this.pos.y) / 100
+      );
+      return steer;
     }
-  
-    return new Vector(0, 0, 0, 0);
+
+    return new Vector(0, 0);
   }
 
   separation(fishArray) {
-    let steer = new Vector(0, 0, 0, 0);
+    let steer = new Vector(0, 0);
     let desiredSeparation = 25;
-  
+
     for (let other of fishArray) {
       if (other !== this) {
-        let diff = new Vector(
-          0,
-          0,
-          this.pos.startX - other.pos.startX,
-          this.pos.startY - other.pos.startY
-        );
-        let d = diff.length;
-  
+        let diff = new Vector(this.pos.x - other.pos.x, this.pos.y - other.pos.y);
+        let d = diff.length();
         if (d < desiredSeparation && d > 0) {
           diff = diff.normalize();
           steer = steer.add(diff);
         }
       }
     }
-  
+
     return steer;
   }
-  alignment(fishArray){
-    let pVel = new Vector(0,0,0,0);
+
+  alignment(fishArray) {
+    let avgVel = new Vector(0, 0);
     let total = 0;
     let chgVelocity = 10;
-    for(let other of fishArray){
-      if(other !== this){
-        pVel = pVel.add(other.vel);
+
+    for (let other of fishArray) {
+      if (other !== this) {
+        avgVel = avgVel.add(other.vel);
         total++;
       }
     }
 
     if (total > 0) {
-      pVel = pVel.div(total); 
-      let steer = pVel.subtract(this.vel);
-      steer = steer.div(chgVelocity);
+      avgVel = avgVel.div(total);
+      let steer = avgVel.subtract(this.vel).div(chgVelocity);
       return steer;
-    } else {
-      return new Vector(0,0,0,0);
     }
+
+    return new Vector(0, 0);
   }
+
   update(fishArray) {
-    // Get steering forces
     let coh = this.cohesion(fishArray);
     let sep = this.separation(fishArray);
     let ali = this.alignment(fishArray);
-  
-    // Weight the forces
+
     let steer = coh.add(sep).add(ali);
-  
-    // Add to velocity
     this.vel = this.vel.add(steer);
-  
-    // Limit velocity (optional but helps)
+
     let maxSpeed = 5;
-    if (this.vel.length > maxSpeed) {
+    if (this.vel.length() > maxSpeed) {
       this.vel = this.vel.normalize().div(1 / maxSpeed);
     }
-  
-    // Update position
+
     this.pos = this.pos.add(this.vel);
   }
 }
+
 
 let fishArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for(let i = 0;i < 50; i++){
+  for (let i = 0; i < 50; i++) {
     fishArray.push(new PreyFish());
   }
 }
 
 function draw() {
-  background(0,0,220);
-  
-  for(i = 0; i < fishArray.length; i++){
+  background(0, 0, 220);
+  for (let i = 0; i < fishArray.length; i++) {
     let fish = fishArray[i];
     fish.update(fishArray);
     fish.show();
-  } 
+  }
 }
